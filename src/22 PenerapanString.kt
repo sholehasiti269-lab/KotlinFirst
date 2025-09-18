21b556fc91f2866a0a5b0de54c3b5578b6ddd4cd
sholehasiti269-lab <sholehasiti269@gmail.com>
2025-09-18 19:48:19 +0700

    pratukum modul 1 saya

--- src/22 PenerapanString.kt
+++ src/22 PenerapanString.kt
@@ -1,28 +1,25 @@
 fun main(args: Array<String>) {
-    //character occurence in a sentence analysis
-    //the string that we want to analyze
-    var s = "A programer gets struck in the shower because the intructions one the shampoo were:lather,wash,and repeat"
+// Character occurrence in a sentence analysis
+// the string that we want to analyze
+    var s = "A programmer gets stuck in the shower because the instructions on the shampoo were: Lather,Wash, and Repeat."
     println(s)
     s = s.toString()
-
-    //counters initialization
+// counters initialization
     var vowelCount = 0
     var consonantCount = 0
-
-    //definition of character groups
+// definition of character groups
     val vowels = "aeiouy"
-    val consonants = "bcdfgjklmnpqrstvwxz"
-
-    //main loop
+    val consonants = "bcdfghjklmnpqrstvwxz"
+// main loop
     for (c in s) {
-        if (vowels.contains(c))
+        if (vowels.contains(c)) {
             vowelCount++
-        else if (consonants.contains(c)) {
+        } else if (consonants.contains(c)) {
             consonantCount++
-
-            println("Vowels: $vowelCount")
-            println("Consonants: $consonantCount")
-            println("Other characters: ${s.length - (vowelCount + consonantCount)}")
         }
     }
+
+    println("Vowels: $vowelCount")
+    println("Consonants: $consonantCount")
+    println("Other characters: ${s.length - (vowelCount + consonantCount)}")
 }
